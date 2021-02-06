fa8f4eae5d1c5a3c2bfaec8dde9b74467bd63ca8
Basant Sharma <scriptclump@gmail.com>
2021-02-06 12:22:41 +0530

    Adds the auth & appointment deployments file

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -4,6 +4,6 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
-app.listen(3000, () => {
-    console.log('Listening to port 3000');
-});
+app.listen(3001, () => {
+    console.log('Listening to port 3001');
+})
