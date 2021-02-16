d250948371017795b8de4144df80ea3ece36ce7a
Basant Sharma <scriptclump@gmail.com>
2021-02-16 10:23:58 +0530

    Adds the routes for signin, signup & singout

---
+++ services/auth/src/routes/signup.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+
+const router = express.Router();
+router.get('/api/users/signup', [
+    body('email')
+        .isEmail()
+        .withMessage('Email must be valid'),
+    body('password')
+        .trim()
+        .isLength({ min: 4, max:20 })
+        .withMessage('Password must be between 4 to 20 chatacters')
+], (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(400).send(errors.array())
+    }
+
+    const { email, passowrd } = req.body;
+
+    if(!email || typeof email !== 'string'){
+        res.status(400).send('Provide a valid email');
+    }
+});
+export { router as signupRouter }
