b5e9cef8ded54d48d8673ca7ff4be7c3bd97a90d
Basant Sharma <scriptclump@gmail.com>
2021-03-26 13:38:47 +0530

    Adds the Errorss & middleware

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -5,6 +5,7 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 app.use(json());
--- services/auth/src/routes/signin.ts
+++ services/auth/src/routes/signin.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
 router.get('/api/users/signin', (req, res) => {
-    res.send('Hi there !!');
+    res.send('Hi there 2!!');
 });
 export { router as signinRouter }
