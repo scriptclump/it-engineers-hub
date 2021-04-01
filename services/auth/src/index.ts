17ef88cf2d53f8a1a2e50bd7566fab157e1c3ee6
Basant Sharma <scriptclump@gmail.com>
2021-04-01 12:04:05 +0530

    Adds the mongo database in auth mircoservice

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { json } from 'body-parser';
+//import { mongoose } from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
--- services/auth/src/routes/signup.ts
+++ services/auth/src/routes/signup.ts
@@ -2,14 +2,14 @@ import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
-router.get('/api/users/signup', [
+router.post('/api/users/signup', [
     body('email')
         .isEmail()
         .withMessage('Email must be valid'),
     body('password')
         .trim()
-        .isLength({ min: 4, max:20 })
-        .withMessage('Password must be between 4 to 20 chatacters')
+        .isLength({ min: 8, max:20 })
+        .withMessage('Password must be between 8 to 20 chatacters')
 ], (req: Request, res: Response) => {
     const errors = validationResult(req);
 
